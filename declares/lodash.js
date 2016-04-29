4286c3a1a691e4099d080ee44d1b6450366a4a43
Tsubasa Namiki <namikingsoft@gmail.com>
2016-04-29 22:20:20 +0900

    refactor: function type more strict

--- declares/lodash.js
+++ declares/lodash.js
@@ -1,6 +1,6 @@
 declare module "lodash" {
   declare class Lodash {
-    map<T>(list: Array<T>, func: Function): Array<T>
+    map<T>(list: Array<T>, func: (x:T)=>T): Array<T>
   }
 
   declare var exports: Lodash;
