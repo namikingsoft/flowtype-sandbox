45061d51b6a722df047bc228bd69ca8cee8a01db
Tsubasa Namiki <namikingsoft@gmail.com>
2016-05-04 17:40:06 +0900

    style: change to double quote from single on test

--- test/mylibTest.js
+++ test/mylibTest.js
@@ -1,14 +1,14 @@
 /* @flow */
 /* eslint-disable */
 
-import assert from 'power-assert'
-import MyLib from 'mylib'
+import assert from "power-assert"
+import MyLib from "mylib"
 
-describe('MyLib', () => {
-  describe('method', () => {
-    it('should be return string number', () => {
+describe("MyLib", () => {
+  describe("method", () => {
+    it("should be return string number", () => {
       const mylib = new MyLib()
-      assert(mylib.method(1234) === '1234')
+      assert(mylib.method(1234) === "1234")
     })
   })
 })
