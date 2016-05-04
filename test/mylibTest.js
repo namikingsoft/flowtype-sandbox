270b7065627efae28e98940f0f37e08a9de5195c
Tsubasa Namiki <namikingsoft@gmail.com>
2016-05-04 16:32:23 +0900

    test: add mylib test

---
+++ test/mylibTest.js
@@ -0,0 +1,14 @@
+/* @flow */
+/* eslint-disable */
+
+import assert from 'power-assert'
+import MyLib from 'mylib'
+
+describe('MyLib', () => {
+  describe('method', () => {
+    it('should be return string number', () => {
+      const mylib = new MyLib()
+      assert(mylib.method(1234) === '1234')
+    })
+  })
+})
