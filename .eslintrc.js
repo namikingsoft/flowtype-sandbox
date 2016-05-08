49bb54464a736bf6bb55f6770d943c3c332ba22a
Tsubasa Namiki <namikingsoft@gmail.com>
2016-05-08 23:39:47 +0900

    chore: incorporate flow-vars plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,14 @@ module.exports = {
   "extends": "airbnb",
   "plugins": [
     "react",
+    "flow-vars",
   ],
   "rules": {
     "semi": [2, "never"],
     "quotes": [2, "double"],
     "space-before-function-paren": 0,
+    "flow-vars/define-flow-type": 1,
+    "flow-vars/use-flow-type": 1,
   },
   "env": {
     "browser": true,
